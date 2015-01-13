34f6bbdb88ba9b15c0d2fa7542e297bacd51dcf1
Will Carter <whcarter3@gmail.com>
2015-01-13 11:06:48 -0800

    add validation to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      session[:user_id] = @user.id.to_s
   		redirect_to posts_path
   	else
   		render :new
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User
   field :email, type: String
   field :password_digest, type: String
 
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+  validates :password, presence: true, confirmation: true, length: { in: 6..20 }
+
+  attr_reader :password
+
   def password=(unencrypted_password)
   	self.password_digest = BCrypt::Password.create(unencrypted_password)
   end
